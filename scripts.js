fbf75e233f8d0ef4214f306eb8658852b38390ec
TishuFR <a.aldebert@intech-sud.fr>
2020-12-11 14:58:31 +0100

    debugging de ouf v2

--- scripts.js
+++ scripts.js
@@ -1,32 +1,31 @@
-
 //Jquery pour le menu burger 
-$(function(){
-  $(".header_usermenu_responsive-icon i").click(function(){
-    $(".header_usermenu").toggleClass("clicked");
-  });
-  
-  $(".main-menu-responsive-icon i").click(function(){
-    $(".main-menu .mobile-hide").toggleClass("clicked");
-  });
+$(function() {
+    $(".header_usermenu_responsive-icon i").click(function() {
+        $(".header_usermenu").toggleClass("clicked");
+    });
+
+    $(".main-menu-responsive-icon i").click(function() {
+        $(".main-menu .mobile-hide").toggleClass("clicked");
+    });
 });
 
 //Chargement de l'image pour la bannière
 window.onload = function() {
-  document.body.className += ' loaded'
+    document.body.className += ' loaded'
 };
 
 // footer map function JS
-(function () {
-  var setting = {"height":200,"width":500,"zoom":15,"queryString":"Les Deux Tours, Brignon, France","place_id":"ChIJ5_9ORLQ5tBIR_A_PFzuXsbU","satellite":false,"centerCoord":[43.97842839405216,4.21104855632469],"cid":"0xb5b1973b17cf0ffc","lang":"fr","cityUrl":"/france/nimes-2733","cityAnchorText":"Carte de Nîmes, Sud de la France, France","id":"footer-map-body","embed_id":"297355"};
-  var d = document;
-  var s = d.createElement('script');
-  s.src = 'https://1map.com/js/script-for-user.js?embed_id=297355';
-  s.async = true;
-  s.onload = function (e) {
-      window.OneMap.initMap(setting);
-}
-  var to = d.getElementsByTagName('script')[0];
-  to.parentNode.insertBefore(s, to);
+(function() {
+    var setting = { "height": 200, "width": 500, "zoom": 15, "queryString": "Les Deux Tours, Brignon, France", "place_id": "ChIJ5_9ORLQ5tBIR_A_PFzuXsbU", "satellite": false, "centerCoord": [43.97842839405216, 4.21104855632469], "cid": "0xb5b1973b17cf0ffc", "lang": "fr", "cityUrl": "/france/nimes-2733", "cityAnchorText": "Carte de Nîmes, Sud de la France, France", "id": "footer-map-body", "embed_id": "297355" };
+    var d = document;
+    var s = d.createElement('script');
+    s.src = 'https://1map.com/js/script-for-user.js?embed_id=297355';
+    s.async = true;
+    s.onload = function(e) {
+        window.OneMap.initMap(setting);
+    }
+    var to = d.getElementsByTagName('script')[0];
+    to.parentNode.insertBefore(s, to);
 })();
 
 // gallery function JS
@@ -37,32 +36,35 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("galerie_slides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+    var i;
+    var slides = document.getElementsByClassName("galerie_slides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
-
-
+//Fonction de remplacement du bouton d'envoi de mail
+function EnvoiMail() {
+    // appel du node mailer
+    document.getElementById("bouton").value = "Message envoyé ✔";
+}
